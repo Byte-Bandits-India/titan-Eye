dec4c205166aa4b14d8c084b22afc43f225be5c2
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-06-20 16:32:31 +0530

    fix: disable Nginx proxy buffering for SSE stream by adding X-Accel-Buffering: no header

--- server/index.ts
+++ server/index.ts
@@ -43,8 +43,9 @@ app.get('/api/ping', (req: Request, res: Response) => {
 app.get('/api/events', (req: Request, res: Response) => {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
+    'Cache-Control': 'no-cache, no-transform',
+    'Connection': 'keep-alive',
+    'X-Accel-Buffering': 'no'
   });
   res.write('\n');
 
