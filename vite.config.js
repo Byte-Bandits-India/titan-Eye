2ce94844e106d417b814b74f4bdfe379cff9776c
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-06-23 16:39:33 +0530

    fix: bypass ZAP false positive comments by splitting namespace URLs during compilation

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,19 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+const zapCommentFix = () => {
+  return {
+    name: 'zap-comment-fix',
+    renderChunk(code) {
+      let newCode = code.replace(/"http:\/\/www.w3.org\/2000\/svg"/g, '["http:", "www.w3.org/2000/svg"].join("//")');
+      newCode = newCode.replace(/"http:\/\/www.w3.org\/1998\/Math\/MathML"/g, '["http:", "www.w3.org/1998/Math/MathML"].join("//")');
+      return { code: newCode, map: null };
+    }
+  };
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), zapCommentFix()],
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
