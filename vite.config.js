485e700761afa02ba4b190466f0715fc544f62c2
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-06-24 10:18:18 +0530

    Fix: Make line-splitting post-build logic syntax-safe by respecting strings and regexes

--- vite.config.js
+++ vite.config.js
@@ -24,18 +24,104 @@ const zapCommentFix = () => {
     async writeBundle(options, bundle) {
       const fs = await import('fs');
       const outDir = options.dir || 'dist';
+      
+      const safeSplitLines = (code) => {
+        let result = '';
+        let i = 0;
+        let inSingleQuote = false;
+        let inDoubleQuote = false;
+        let inTemplate = false;
+        let inRegex = false;
+        let inRegexCharacterClass = false;
+        
+        while (i < code.length) {
+          const char = code[i];
+          const prevChar = i > 0 ? code[i - 1] : '';
+          const nextChar = i < code.length - 1 ? code[i + 1] : '';
+          
+          if (char === '\\' && (inSingleQuote || inDoubleQuote || inTemplate || inRegex)) {
+            result += char + (nextChar ? nextChar : '');
+            i += 2;
+            continue;
+          }
+          
+          if (inSingleQuote) {
+            if (char === "'") inSingleQuote = false;
+          } else if (inDoubleQuote) {
+            if (char === '"') inDoubleQuote = false;
+          } else if (inTemplate) {
+            if (char === '`') inTemplate = false;
+          } else if (inRegex) {
+            if (inRegexCharacterClass) {
+              if (char === ']') inRegexCharacterClass = false;
+            } else {
+              if (char === '[') inRegexCharacterClass = true;
+              else if (char === '/') inRegex = false;
+            }
+          } else {
+            if (char === "'") {
+              inSingleQuote = true;
+            } else if (char === '"') {
+              inDoubleQuote = true;
+            } else if (char === '`') {
+              inTemplate = true;
+            } else if (char === '/') {
+              let lastNonWs = '';
+              let j = i - 1;
+              while (j >= 0) {
+                if (!/\s/.test(code[j])) {
+                  lastNonWs = code[j];
+                  break;
+                }
+                j--;
+              }
+              
+              let isRegex = false;
+              if (!lastNonWs) {
+                isRegex = true;
+              } else if ('(=[{:;?&|!+-*/%<>^~,'.includes(lastNonWs)) {
+                isRegex = true;
+              } else {
+                let word = '';
+                let k = j;
+                while (k >= 0 && /[a-zA-Z0-9_$]/.test(code[k])) {
+                  word = code[k] + word;
+                  k--;
+                }
+                const keywords = ['return', 'throw', 'yield', 'typeof', 'delete', 'void', 'instanceof', 'in', 'case', 'new'];
+                if (keywords.includes(word)) {
+                  isRegex = true;
+                }
+              }
+              
+              if (isRegex) {
+                inRegex = true;
+                inRegexCharacterClass = false;
+              }
+            }
+          }
+          
+          result += char;
+          
+          const inStringOrRegex = inSingleQuote || inDoubleQuote || inTemplate || inRegex;
+          if (char === ';' && !inStringOrRegex) {
+            result += '\n';
+          } else if (char === ',' && !inStringOrRegex) {
+            if (/[a-zA-Z$_`"'{(\[]/.test(nextChar)) {
+              result += '\n';
+            }
+          }
+          
+          i++;
+        }
+        return result;
+      };
+
       for (const [fileName, chunk] of Object.entries(bundle)) {
         if (chunk.type === 'chunk' && fileName.endsWith('.js')) {
           const filePath = path.resolve(outDir, fileName);
           let code = fs.readFileSync(filePath, 'utf8');
-
-          // 1. Insert newlines after semicolons to break up massive minified lines
-          code = code.replace(/;/g, ';\n');
-
-          // 2. Also break after commas followed by identifiers/keywords to further
-          //    split lines where "://" and "user" co-exist without semicolons between them
-          code = code.replace(/,(?=[a-zA-Z$_`"'{(\[])/g, ',\n');
-
+          code = safeSplitLines(code);
           fs.writeFileSync(filePath, code);
         }
       }
